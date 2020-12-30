40712b81997440a9693a0c880a24ffe4e09a11ad
Jaime Still <jpstill@gmail.com>
2020-12-30 11:58:54 -0500

    modules chapter complete

---
+++ restaurant/src/front_of_house.rs
@@ -0,0 +1,10 @@
+// make hosting public to anything that can access front_of_house
+pub mod hosting;
+
+mod serving {
+    fn take_order() { }
+
+    fn serve_order() { }
+
+    fn take_payment() { }
+}
---
+++ restaurant/src/lib.rs
@@ -0,0 +1,71 @@
+/*
+    Using a semi-colon here rather than a block
+    tells Rust to load the contents of the module
+    from another file with the same name as
+    the module.
+*/
+mod front_of_house;
+
+pub mod back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad
+    }
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches")
+            }
+        }
+    }
+
+    fn cook_order() { }
+
+    fn fix_incorrect_order() {
+        cook_order();
+
+        // relative path to serve_order() defined up one level
+        super::serve_order();
+    }
+}
+
+fn serve_order() { }
+
+// bring the hosting namespace into scope
+use crate::front_of_house::hosting;
+
+/*
+    bring the back_of_house namespace into scope, 
+    as well as Breakfast and Appetizer, 
+    using nested path and setting an alias for
+    Breakfast as Bfast
+
+    NOTE: to also include the specified namespace, use self:
+    use crate::module::sub_module::{self, ...}
+*/
+use crate::back_of_house::{ Breakfast as Bfast, Appetizer };
+
+pub fn eat_at_restaurant() {
+    // absolute path
+    // crate::front_of_house::hosting::add_to_waitlist();
+
+    // relative path
+    // front_of_house::hosting::add_to_waitlist();
+
+    // call add_to_waitlist() from the hosting namespace
+    hosting::add_to_waitlist();
+
+    let mut meal = Bfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    let order1 = Appetizer::Soup;
+    let order2 = Appetizer::Salad;
+}
